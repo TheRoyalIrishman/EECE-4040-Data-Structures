70f8eca87b3fb8377bc16ebcc0cf9d3e2925b7c6
TheIrishBearcat24 <61879913+TheIrishBearcat24@users.noreply.github.com>
2023-03-03 09:55:48 -0500

    Fixed up inorder print traversal

--- Book.cpp
+++ Book.cpp
@@ -37,11 +37,17 @@ void Book::addEntry(Person newPerson) {
 // on how to do an inorder traversal of a BST
 void Book::inorderTraversal(BST_Node * nodePtr) const {
     if (nodePtr != nullptr) {
-        inorderTraversal(nodePtr->left);
+        if (nodePtr->left) {
+            inorderTraversal(nodePtr);
+        }
+
         cout << "First Name: " << nodePtr->m_person.firstName << endl;
         cout << "Last Name: " << nodePtr->m_person.lastName << endl;
         cout << "Phone Number: " << nodePtr->m_person.phoneNumber << endl;
-        inorderTraversal(nodePtr->right);
+
+        if (nodePtr->right) {
+            inorderTraversal(nodePtr);
+        }
     }
 }
 
