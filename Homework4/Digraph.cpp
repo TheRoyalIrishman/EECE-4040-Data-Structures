5f4dff3a2621523a177c9e4b89c6caec9d102a20
William Baker <bbrk24@gmail.com>
2023-03-08 09:14:24 -0500

    Fix representation to match project spec

--- Homework4/Digraph.cpp
+++ Homework4/Digraph.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
-#include <unordered_map>
+#include <utility>
 #include <stack>
 #include <vector>
 #include <list>
 
 using namespace std;
 
-struct DAG_Node {
-
-};
-
 class Digraph {
     private:
-        unordered_map<int, vector<int>> digraph; // DAG variable
-    
+        using node = pair<int, list<int>>;
+        vector<node> m_graph;
+
     public:
         // true if it succeeds, false if it fails
         bool addEdge(int first, int second);
