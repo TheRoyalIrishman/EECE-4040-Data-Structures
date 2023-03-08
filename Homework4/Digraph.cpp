e2d38fd272d3fb632438748dda21fa1bf95bcba6
William Baker <bbrk24@gmail.com>
2023-03-08 09:19:27 -0500

    add indexOf (linear search)

--- Homework4/Digraph.cpp
+++ Homework4/Digraph.cpp
@@ -11,6 +11,16 @@ class Digraph {
         using node = pair<int, list<int>>;
         vector<node> m_graph;
 
+        // returns -1 if not found
+        int indexOf(int node) {
+            for (int i = 0; i < m_graph.size(); ++i) {
+                if (m_graph[i].first == node) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
     public:
         // true if it succeeds, false if it fails
         bool addEdge(int first, int second);
