4d49edc4293a27371ae7c32322c86ce24e159d4f
William Baker <bbrk24@gmail.com>
2023-03-08 09:35:10 -0500

    implement addEdge

--- Homework4/Digraph.cpp
+++ Homework4/Digraph.cpp
@@ -3,6 +3,7 @@
 #include <stack>
 #include <vector>
 #include <list>
+#include <unordered_set>
 
 using namespace std;
 
@@ -12,7 +13,7 @@ class Digraph {
         vector<node> m_graph;
 
         // returns -1 if not found
-        int indexOf(int node) {
+        int indexOf(int node) const {
             for (int i = 0; i < m_graph.size(); ++i) {
                 if (m_graph[i].first == node) {
                     return i;
@@ -21,9 +22,54 @@ class Digraph {
             return -1;
         }
 
+        // determine whether the given node is part of a cycle
+        bool hasCycle(int start, unordered_set<int>& visited) const {
+            int index = indexOf(start);
+            if (index == -1) {
+                return false;
+            }
+
+            visited.insert(start);
+            for (int next : m_graph[index].second) {
+                if (visited.find(next) != visited.end()) {
+                    return true;
+                }
+                if (hasCycle(next, visited)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        bool hasCycle(int start) const {
+            unordered_set<int> visited;
+            return hasCycle(start, visited);
+        }
     public:
         // true if it succeeds, false if it fails
-        bool addEdge(int first, int second);
+        bool addEdge(int first, int second) {
+            // find or add the first node
+            int index = indexOf(first);
+            if (index == -1) {
+                index = m_graph.size();
+                m_graph.emplace_back(first, list<int>());
+            }
+
+            // add the second node if it doesn't exist
+            if (indexOf(second) == -1) {
+                m_graph.emplace_back(second, list<int>());
+            }
+
+            m_graph[index].second.push_front(second);
+
+            // ensure there's no cycle
+            if (hasCycle(first)) {
+                // oops
+                m_graph[index].second.pop_front();
+                return false;
+            }
+            return true;
+        }
         void removeEdge(int first, int second);
         list<int> topologicalSort() const;
 };
